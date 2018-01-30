194d54cab51c2115882aaac7d4505d874e45ed52
Kailuo Wang <kailuo.wang@gmail.com>
2018-01-30 16:03:23 -0500

    finally figured out the DAO creation DAO, right?

--- mongo/src/main/scala/lihua/mongo/DAOFactory.scala
+++ mongo/src/main/scala/lihua/mongo/DAOFactory.scala
@@ -6,7 +6,6 @@ import play.api.libs.json.Format
 import reactivemongo.api.ReadPreference
 import reactivemongo.play.json.collection.JSONCollection
 import Mongo._
-import cats.data.EitherT
 import cats.effect.{Async, IO}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -14,35 +13,34 @@ import net.ceedubs.ficus.Ficus._
 import cats.effect.implicits._
 import cats.implicits._
 
-trait DAOFactory[F[_], A] {
+/**
+ * Should be created one per application
+ */
+class DAOFactory[F[_]: Async](cfg: Config)(implicit sh: ShutdownHook) {
 
-  type Result[T] = EitherT[F, DBError, T]
+  val mongoF: F[Mongo] = IO(Mongo(cfg)).liftIO
 
-  def ensure(collection: JSONCollection)(implicit ec: ExecutionContext): F[Unit]
-  def createFromCollection(collection: JSONCollection)(implicit ec: ExecutionContext, format: Format[A]): EntityDAO[Result, A]
-
-  def create(cfg: Config, dbName: String, collectionName: String)(implicit ec: ExecutionContext,
-                                                 sh: ShutdownHook,
-                                                 F: Async[F],
-                                                 formatT: Format[A]): F[EntityDAO[Result, A]] = {
+  def create(dbName: String, collectionName: String)
+            (implicit ec: ExecutionContext): F[JSONCollection] = {
     val readPreference = cfg.as[Option[String]]("mongoDB.read-preference." + collectionName).fold[ReadPreference](ReadPreference.primary) {
       case "secondary" => ReadPreference.secondary
       case "primary"   => ReadPreference.primary
     }
 
     for {
-      db <- toF(Mongo(cfg).database(dbName))
-      collection = db.jsonCollection(collectionName, readPreference = readPreference)
-      _ <- ensure(collection)
-    } yield createFromCollection(collection)
+      mongo <- mongoF
+      db <- toF(mongo.database(dbName))
+    } yield db.jsonCollection(collectionName, readPreference = readPreference)
+
   }
 
+  def createDAO[RF[_], A: Format](dbName: String,
+                                  collectionName: String,
+                                  f: JSONCollection => EntityDAO[RF, A])
+                                 (implicit ec: ExecutionContext): F[EntityDAO[RF, A]] =
+    create(dbName, collectionName).map(f)
 
-
-  protected def toF[B](f : => Future[B])(implicit F: Async[F], ec: ExecutionContext) : F[B] =
+  protected def toF[B](f : => Future[B])(implicit ec: ExecutionContext) : F[B] =
     IO.fromFuture(IO(f)).liftIO
 }
 
-trait IODAOFactory[A] extends DAOFactory[IO, A] {
-  def createFromCollection(collection: JSONCollection)(implicit ec: ExecutionContext, format: Format[A]): EntityDAO[Result, A] = new IOEntityDAO[A](collection)
-}
