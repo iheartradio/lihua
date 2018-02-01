096c4cf6e08410c347c4266dea0e2b8e0822797d
Kailuo Wang <kailuo.wang@gmail.com>
2018-02-01 09:54:42 -0500

    replace the immature jvm shutdownhook with a naive manual one.

--- mongo/src/main/scala/lihua/mongo/ShutdownHook.scala
+++ mongo/src/main/scala/lihua/mongo/ShutdownHook.scala
@@ -10,10 +10,12 @@ trait ShutdownHook {
 
 
 object ShutdownHook {
-  object jvmShutdownHook extends ShutdownHook {
-    def onShutdown[T](code: ⇒ T): Unit = {
-      sys.addShutdownHook({code; ()})
-      ()
+  object manual extends ShutdownHook {
+    @volatile
+    private var callbacks: List[() => _] = Nil
+    override def onShutdown[T](code: => T): Unit = {
+      callbacks = (() => code) :: callbacks
     }
+    def shutdown(): Unit = callbacks.foreach(_())
   }
 }
