dd5d88623629be7f7b018494e308553510429056
Kailuo Wang <kailuo.wang@gmail.com>
2018-02-21 21:32:55 -0500

    fix syntax bug

--- mongo/src/main/scala/lihua/mongo/EntityDAO.scala
+++ mongo/src/main/scala/lihua/mongo/EntityDAO.scala
@@ -56,18 +56,18 @@ object EntityDAO {
     implicit def fromSelector(selector: JsObject): Query = Query(selector)
 
     implicit def fromField1[A : Writes](tp: (Symbol, A)): Query =
-      Query(Json.obj(tp._1.toString -> Json.toJson(tp._2)))
+      Query(Json.obj(tp._1.name -> Json.toJson(tp._2)))
 
     implicit def fromFields2[A : Writes, B: Writes](p: ((Symbol, A), (Symbol, B))): Query = p match {
-      case ((s1, a), (s2, b)) => Query(Json.obj(s1.toString -> a, s2.toString -> b))
+      case ((s1, a), (s2, b)) => Query(Json.obj(s1.name -> a, s2.name -> b))
     }
 
     implicit def fromFields3[A : Writes, B: Writes, C: Writes](p: ((Symbol, A), (Symbol, B), (Symbol, C))): Query = p match {
-      case ((s1, a), (s2, b), (s3, c)) => Query(Json.obj(s1.toString -> a, s2.toString -> b, s3.toString -> c))
+      case ((s1, a), (s2, b), (s3, c)) => Query(Json.obj(s1.name -> a, s2.name -> b, s3.name -> c))
     }
 
     implicit def fromFields4[A : Writes, B: Writes, C: Writes, D: Writes](p: ((Symbol, A), (Symbol, B), (Symbol, C), (Symbol, D))): Query = p match {
-      case ((s1, a), (s2, b), (s3, c), (s4, d)) => Query(Json.obj(s1.toString -> a, s2.toString -> b, s3.toString -> c, s4.toString -> d))
+      case ((s1, a), (s2, b), (s3, c), (s4, d)) => Query(Json.obj(s1.name -> a, s2.name -> b, s3.name -> c, s4.name -> d))
     }
 
     implicit def fromId(id: ObjectId): Query = idSelector(id)
--- mongo/src/test/scala/lihua/mongo/QuerySyntaxTests.scala
+++ mongo/src/test/scala/lihua/mongo/QuerySyntaxTests.scala
@@ -1,6 +1,9 @@
 package lihua.mongo
 
+import lihua.mongo.EntityDAO.Query
 import org.scalatest.{FunSuite, Matchers}
+import play.api.libs.json.Json
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class QuerySyntaxTests extends FunSuite with Matchers {
@@ -11,6 +14,8 @@ class QuerySyntaxTests extends FunSuite with Matchers {
     testDAO.find(('a -> 1, 'b -> "d"))
     testDAO.findOne(('a -> 1, 'b -> "d"))
     testDAO.findOne(('a -> 1, 'b -> 2.0, 'c -> "dfd", 'd -> true))
+
+    (('a -> 1, 'b -> "d"):Query) shouldBe (Json.obj("a" -> 1, "b" -> "d"):Query)
     succeed
   }
 }
