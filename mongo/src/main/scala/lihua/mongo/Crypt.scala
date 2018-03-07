29d1c2ae03b6995c58b4b4048378d98143e13e1d
Kailuo Wang <kailuo.wang@gmail.com>
2018-03-07 12:15:39 -0500

    added autoFunctorK to Crypt

--- mongo/src/main/scala/lihua/mongo/Crypt.scala
+++ mongo/src/main/scala/lihua/mongo/Crypt.scala
@@ -1,5 +1,8 @@
 package lihua.mongo
 
+import mainecoon.{autoFunctorK, finalAlg}
+
+@autoFunctorK(autoDerivation = true) @finalAlg
 trait Crypt[F[_]] {
   def encrypt(value: String): F[String]
   def decrypt(value: String): F[String]
