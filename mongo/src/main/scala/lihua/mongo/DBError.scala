b845300d07e11cfb923411b4e8848c665efe8f4c
Kai(luo) Wang <kailuo.wang@gmail.com>
2020-01-22 12:16:29 -0500

    minior improvmeents on error reporting (#102)

--- mongo/src/main/scala/lihua/mongo/DBError.scala
+++ mongo/src/main/scala/lihua/mongo/DBError.scala
@@ -3,21 +3,28 @@ package mongo
 
 import cats.data.NonEmptyList
 
-sealed trait DBError extends Throwable with Product with Serializable
+sealed trait DBError
+    extends RuntimeException
+    with Product
+    with Serializable
 
 object DBError {
-
   case object NotFound extends DBError
 
-  case class DBException(throwable: Throwable, collection: String) extends DBError {
+  case class DBException(
+      throwable: Throwable,
+      collection: String)
+      extends DBError {
     override def getCause: Throwable = throwable
 
-    override def getMessage: String =  s"Error occurred (collection: $collection): ${throwable.getMessage} "
+    override def getMessage: String =
+      s"Error occurred (collection: $collection): ${throwable.getMessage} "
   }
   case class DBLastError(override val getMessage: String) extends DBError
 
-  case class WriteError(details: NonEmptyList[WriteErrorDetail]) extends DBError {
-    override def getMessage: String =  details.toString()
+  case class WriteError(details: NonEmptyList[WriteErrorDetail])
+      extends DBError {
+    override def getMessage: String = details.toString()
   }
 
   sealed trait WriteErrorDetail extends Product with Serializable {
@@ -26,12 +33,20 @@ object DBError {
     override def toString: String = s"code: $code, message: $msg"
   }
 
-  case class ItemWriteErrorDetail(code: Int, msg: String) extends WriteErrorDetail
-  case class WriteConcernErrorDetail(code: Int, msg: String) extends WriteErrorDetail
+  case class ItemWriteErrorDetail(
+      code: Int,
+      msg: String)
+      extends WriteErrorDetail
+  case class WriteConcernErrorDetail(
+      code: Int,
+      msg: String)
+      extends WriteErrorDetail
 
-  case class UpdatedCountErrorDetail(expectedCount: Int, actual: Int) extends DBError {
-    override def getMessage = s"updated count is $actual, expected $expectedCount"
+  case class UpdatedCountErrorDetail(
+      expectedCount: Int,
+      actual: Int)
+      extends DBError {
+    override def getMessage =
+      s"updated count is $actual, expected $expectedCount"
   }
-
-
 }
