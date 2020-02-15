5b1b91f82061b7a1aabf89bbf5a4e71f03f117d6
Kai(luo) Wang <kailuo.wang@gmail.com>
2020-02-14 20:10:33 -0500

    enhance dynamo api (#113)

--- dynamoTestkit/src/main/scala/lihua/dynamo/testkit/LocalDynamo.scala
+++ dynamoTestkit/src/main/scala/lihua/dynamo/testkit/LocalDynamo.scala
@@ -1,14 +1,16 @@
 package lihua.dynamo.testkit
 
 import cats.effect.{Async, Resource, Sync}
-import lihua.dynamo.ScanamoEntityDAO
+import lihua.dynamo.ScanamoManagement
 import org.scanamo.LocalDynamoDB
 import cats.implicits._
 import cats.Parallel
-import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException
+import com.amazonaws.services.dynamodbv2.model.{
+  ResourceNotFoundException,
+  ScalarAttributeType
+}
 
-object LocalDynamo {
-
+object LocalDynamo extends ScanamoManagement {
   def client[F[_]](implicit F: Sync[F]) =
     Resource.make {
       F.delay(LocalDynamoDB.client())
@@ -17,21 +19,21 @@ object LocalDynamo {
     }
 
   def clientWithTables[F[_]: Parallel](
-      tables: String*
+      tables: (String, Seq[(String, ScalarAttributeType)])*
     )(implicit F: Async[F]
     ) =
     client[F].flatTap { client =>
       Resource.make {
-        tables.toList.parTraverse(
-          ScanamoEntityDAO.ensureTable[F](client, _)
-        )
+        tables.toList.parTraverse {
+          case (tableName, keyAttributes) =>
+            ensureTable[F](client, tableName, keyAttributes, 10L, 10L)
+        }
       } { _ =>
         tables.toList.parTraverse { t =>
-          F.delay(LocalDynamoDB.deleteTable(client)(t)).void.recover {
+          F.delay(LocalDynamoDB.deleteTable(client)(t._1)).void.recover {
             case e: ResourceNotFoundException => ()
           }
         }.void
       }
     }
-
 }
